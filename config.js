66d978049304aa3cdbf94a1e9c6e0ff7640ea80f
sunnychan <sunnnychan@gmail.com>
2020-03-18 14:20:55 +0800

    sunnychan

---
+++ config.js
@@ -0,0 +1,19 @@
+// .vuepress/config.js
+module.exports = {
+  title: 'Spock',  // 设置网站标题
+  description : "",
+  base : '/spock-framework-reference-documentation/',
+  themeConfig: {
+    nav : [
+        { text: 'Github', link: 'http://www.github.com/sunnnychan' }
+    ],
+    sidebar: [
+      {
+        title: 'Spock',   // 必要的
+        path: '/',      // 可选的, 应该是一个绝对路径
+        collapsable: true, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+      }
+    ]
+  }
+}
